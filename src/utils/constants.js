ae32c83c32948ff89712b9667a0072549378870d
Ankita <ankitaag94@gmail.com>
2024-07-25 17:53:11 +0530

    update api endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const API_ENDPOINT = "http://localhost:3000/";
+export const API_ENDPOINT =
+  import.meta.env.VITE_ENDPOINT || "http://localhost:3000/";
 
 export const API_ROUTES = {
   POSTS: `${API_ENDPOINT}posts`,
